582f14d45f2ce2d360c202f1c6fec4e960675c14
veegishx <veegish240@gmail.com>
2021-06-14 19:31:33 +0400

    Added relations between author and book types

--- server/schema/schema.js
+++ server/schema/schema.js
@@ -10,9 +10,9 @@ const {
 
 // dummy data 
 const books = [
-  { name: 'Name of the Wind', genre: 'Fantasy', id: '1' },
-  { name: 'The Final Empire', genre: 'Fantasy', id: '2' },
-  { name: 'The Long Earth', genre: 'Sci-Fi', id: '3' },
+  { name: 'Name of the Wind', genre: 'Fantasy', id: '1', authorId: '1' },
+  { name: 'The Final Empire', genre: 'Fantasy', id: '2', authorId: '2' },
+  { name: 'The Long Earth', genre: 'Sci-Fi', id: '3', authorId: '3' }
 ];
 
 // dummy data
@@ -27,7 +27,13 @@ const BookType = new GraphQLObjectType({
   fields: () => ({
     id: {type: GraphQLID},
     name: {type: GraphQLString},
-    genre: {type: GraphQLString}
+    genre: {type: GraphQLString},
+    author: {
+      type: AuthorType,
+      resolve(parent, args) {
+        return (authors.find((author => author.id === parent.id)))
+      }
+    }
   })
 });
 
